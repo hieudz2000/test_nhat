9f40774f53fdcbf58ffb3e45b81e715a1da23f6b
hieudz2000 <duchieu20002501@gmail.com>
2022-02-19 16:01:34 +0700

    add_home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  root 'static_pages#home'
   resources :books
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
